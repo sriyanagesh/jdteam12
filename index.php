fd2b49c2ca70b4591e8dd0a1632eb77d48c84d09
sriyanagesh <sriyanagesh@gmail.cm>
2016-04-03 13:28:38 -0500

    random commit

--- index.php
+++ index.php
@@ -9,26 +9,26 @@
 			<!-- desktop & tablet header-->
             <div id="header">
 				<div id="logo"><a href="#"><img src="sample_logo.png" alt="Joaquina_logo"/></a>
-         
+
                 </div>
 				<div id="side_header">
 					<h1 class="big_words"><img src="phone_icon.png" style="height:1.25em; width:auto;" /> +1(234)567-8910</h1><br />
 					<h2><a class="big_words" href="#login">Login</a>  |  <a class="big_words" href="#register">Register</a></h2><br />
 					<div><form action="#" method="get">
 						<input class="header_text" type="text" placeholder="search" name="input" />
-					
+
 					</form>
 					</div>
 				</div>
 			</div>
-			
+
 			<!--mobile header & menu-->
             <div id="mobile_header" class="hidden" >
 				<img src="sample_logo.png" alt="Joaquina_logo" id="mobile_logo" />
 				<label for="check_menu"><img src="menu_icon.png" alt="menu_icon" id="menu_icon" /></label>
-            
+
             </div>
-			
+
 			<!--nav menu-->
 			<input id="check_menu" type="checkbox" >
 			<div id="nav_menu" >
@@ -40,15 +40,15 @@
 					<li><a href="#contact">Contact Us</a></li>
 				</ul>
 			</div>
-            
+
 			<div id="content">
 				<!-- put your code here (area below the header)-->
 				<!-- banner image-->
                 <div id="banner">
-				<h1 class="big_words" style:"color:#ffa834;">Welcome to <br />Joaquina Dental Care</h1><br /> 
-				<h4>We care for your smile.</h4>		
+				<h1 class="big_words" style:"color:#ffa834;">Welcome to <br />Joaquina Dental Care</h1><br />
+				<h4>We care for your smile.</h4>
 				</div>
-				
+
                 <!-- services grid-->
                 <div id="grid">
 					<div id="services">
@@ -68,16 +68,16 @@
                             <p>We treat you and your entire family and care for your overall oral health</p></div>
                     	</div>
                     </div>
-                    
+
                     <div id="serv_mobile">
                     	<h3>Services offered</h3>
                     	<p>Cleaning & prevention</p><p>Consultation</p>
                     	<p>Primary Care</p><p>Cosmetic Surgery</p>
-               
+
                     	<p>and <a href="#"><strong>more...</strong></a></p>
                     </div>
 				</div>
-				
+
                 <!-- About section-->
                 <div id="about">
                 	<div id="us_different"><div><img src="clinic_pic.jpg"></div>
@@ -89,37 +89,43 @@
                 	<div id="tip_day">
                     <h2>Tip of the day</h2><br />
                     <hr align="left" style="border-width:1px; width:70%; border-color:white; margin-bottom: 10%;" >
-                    <?php
-						// Connect to the database
-						$con = mysqli_connect("localhost","root","","joaquinadental");
-						// Make sure we connected succesfully
-						if(! $con)
-						{
-						die('Connection Failed'.mysqli_error());
-						}
+										<?php
+								$serverName = "joaquinateam12.database.windows.net";
+								$connectionInfo = array( "Database"=>"Joaquinateam12", "UID"=>"admin12@joaquinateam12", "PWD"=>"Trotters12");
+								$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-						// Select the database to use
-								
-						$tip_num = rand(1,6);
-						$tip = mysqli_query($con,"SELECT Tip_Desc from Joaquinadental.daily_tip where Tip_ID =".$tip_num.";");
-						
-						
-						while($row = mysqli_fetch_assoc($tip))
-						{
-							echo $row["Tip_Desc"];
-						}
-					?>
-					
+								if( $conn ) {
+								echo "Connection established.<br />";
+								}
+								else {
+								die(print_r(sqlsrv_errors(), true));
+								}
+
+							//	mssql_select_db('joaquinateam12',$conn);
+
+								$sql = "select * from dbo.Dentist";
+								$stmt = sqlsrv_query($conn, $sql);
+								if( !$stmt) {
+								die(print_r(sqlsrv_errors(), true));
+								}
+								while($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)){
+								echo $row['Dentist_First_Name'];
+								}
+
+								echo "hello mummy!";
+
+?>
+
                     </div>
                 </div>
-                
-			
+
+
 			</div>
             <div id="footer">
 			Developed by Team 12.
 			</div>
-			
+
 		</div>
-        
+
 </body>
 </html>
