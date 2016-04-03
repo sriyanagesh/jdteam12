96ca13328fe57ad7678188e604b9e945f73b8bb8
sriyanagesh <sriyanagesh@gmail.cm>
2016-04-03 13:59:52 -0500

    ankit commit

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="def_css.css">
-	<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" type="text/css" href="css/def_css.css">
+	<link rel="stylesheet" type="text/css" href="css/home.css">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div id="page">
 			<!-- desktop & tablet header-->
             <div id="header">
-				<div id="logo"><a href="#"><img src="sample_logo.png" alt="Joaquina_logo"/></a>
-
+				<div id="logo"><a href="#"><img src="images/sample_logo.png" alt="Joaquina_logo"/></a>
+         
                 </div>
 				<div id="side_header">
-					<h1 class="big_words"><img src="phone_icon.png" style="height:1.25em; width:auto;" /> +1(234)567-8910</h1><br />
+					<h1 class="big_words"><img src="images/phone_icon.png" style="height:1.25em; width:auto;" /> +1(234)567-8910</h1><br />
 					<h2><a class="big_words" href="#login">Login</a>  |  <a class="big_words" href="#register">Register</a></h2><br />
 					<div><form action="#" method="get">
 						<input class="header_text" type="text" placeholder="search" name="input" />
-
+					
 					</form>
 					</div>
 				</div>
 			</div>
-
+			
 			<!--mobile header & menu-->
             <div id="mobile_header" class="hidden" >
-				<img src="sample_logo.png" alt="Joaquina_logo" id="mobile_logo" />
-				<label for="check_menu"><img src="menu_icon.png" alt="menu_icon" id="menu_icon" /></label>
-
+				<img src="images/sample_logo.png" alt="Joaquina_logo" id="mobile_logo" />
+				<label for="check_menu"><img src="images/menu_icon.png" alt="menu_icon" id="menu_icon" /></label>
+            
             </div>
-
+			
 			<!--nav menu-->
 			<input id="check_menu" type="checkbox" >
 			<div id="nav_menu" >
@@ -40,47 +40,47 @@
 					<li><a href="#contact">Contact Us</a></li>
 				</ul>
 			</div>
-
+            
 			<div id="content">
 				<!-- put your code here (area below the header)-->
 				<!-- banner image-->
                 <div id="banner">
-				<h1 class="big_words" style:"color:#ffa834;">Welcome to <br />Joaquina Dental Care</h1><br />
-				<h4>We care for your smile.</h4>
+				<h1 class="big_words" style:"color:#ffa834;">Welcome to <br />Joaquina Dental Care</h1><br /> 
+				<h4>We care for your smile.</h4>		
 				</div>
-
+				
                 <!-- services grid-->
                 <div id="grid">
 					<div id="services">
                     	<div class="service">
-                    		<div><img src="service_1.jpg"></div>
+                    		<div><img src="images/service_1.jpg"></div>
                     		<div><h3>Cleaning & Prevention</h3>
                             <p>Removal of dental plaque from teeth with the intention of preventing cavities</p></div>
                     	</div>
                     	<div class="service">
-                    		<div><img src="service_2.jpg"></div>
+                    		<div><img src="images/service_2.jpg"></div>
                     		<div><h3>Consultation</h3>
                             <p>Full oral assessment to establish oral health and recommend the best treatments if needed</p></div>
                     	</div>
                         <div class="service">
-                    		<div><img src="service_3.jpg"></div>
+                    		<div><img src="images/service_3.jpg"></div>
                     		<div><h3>Primary Care</h3>
                             <p>We treat you and your entire family and care for your overall oral health</p></div>
                     	</div>
                     </div>
-
+                    
                     <div id="serv_mobile">
                     	<h3>Services offered</h3>
                     	<p>Cleaning & prevention</p><p>Consultation</p>
                     	<p>Primary Care</p><p>Cosmetic Surgery</p>
-
+               
                     	<p>and <a href="#"><strong>more...</strong></a></p>
                     </div>
 				</div>
-
+				
                 <!-- About section-->
                 <div id="about">
-                	<div id="us_different"><div><img src="clinic_pic.jpg"></div>
+                	<div id="us_different"><div><img src="images/clinic_pic.jpg"></div>
                 		<div>
                 		<h2>What makes us different</h2><br />
                 		<p>Here at Joaquina Dental Clinic, our focus is you! We strive for you and your family's dental health and satisfaction. From simple procedures such as teeth cleaning to complicated cases such as Gingivoplasty, we ensure you are taken care of in the best possible way. Walk in to our office to talk to our wonderful staff and to check out the modern treatment rooms and you'll know why we are the best dental clinic in the city. It is our job to make you smile because we all know that a smile is the prettiest thing you can wear.</p>
@@ -89,43 +89,50 @@
                 	<div id="tip_day">
                     <h2>Tip of the day</h2><br />
                     <hr align="left" style="border-width:1px; width:70%; border-color:white; margin-bottom: 10%;" >
-										<?php
+                    <?php
+						
+						
+								
 								$serverName = "joaquinateam12.database.windows.net";
 								$connectionInfo = array( "Database"=>"Joaquinateam12", "UID"=>"admin12@joaquinateam12", "PWD"=>"Trotters12");
 								$conn = sqlsrv_connect( $serverName, $connectionInfo);
-
 								if( $conn ) {
-								echo "Connection established.<br />";
+								
 								}
 								else {
 								die(print_r(sqlsrv_errors(), true));
 								}
-
-							//	mssql_select_db('joaquinateam12',$conn);
-
-								$sql = "select * from dbo.Dentist";
+							
+								$sql = "select Tip_Desc from dbo.daily_tip";
 								$stmt = sqlsrv_query($conn, $sql);
 								if( !$stmt) {
 								die(print_r(sqlsrv_errors(), true));
 								}
+								$k=0;
 								while($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)){
-								echo $row['Dentist_First_Name'];
+								$tip_arr[$k] = $row[$k];
+								$k++;
 								}
+								$tip_num = rand(0, count($tip_arr));
+								$tip = $tip_arr[$tip_num];
+							
+								echo $tip."hello mummy!";
 
-								echo "hello mummy!";
 
 ?>
-
+						
+						
+					
                     </div>
                 </div>
-
-
+                
+			
 			</div>
             <div id="footer">
 			Developed by Team 12.
 			</div>
-
+			
 		</div>
-
+        
 </body>
 </html>
